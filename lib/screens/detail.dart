d5d49f7750d6658c94038260ff925ba9a57ef62a
Mustapha amin <mustaphaamin2003@gmail.com>
2023-01-19 04:21:19 +0100

    modified my dictionary model name and poperties

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,7 +1,7 @@
 import 'package:english_dictionary/services/http_service.dart';
 import 'package:flutter/material.dart';
 
-import '../models/word.dart';
+import '../models/dictionary_model.dart';
 
 class DetailScreen extends StatefulWidget {
   String? entry;
@@ -12,7 +12,7 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
-  late Future<Word> word;
+  late FutureList<<DictionaryModel>> word;
 
   @override
   void initState() {
@@ -25,11 +25,11 @@ class _DetailScreenState extends State<DetailScreen> {
     return Scaffold(
       body: FutureBuilder(
         future: word,
-        builder: (BuildContext context, AsyncSnapshot<Word> snapshot) {
+        builder: (BuildContext context, AsyncSnapshot<List<DictionaryModel>> snapshot) {
           if (snapshot.hasData) {
-            Word word = snapshot.data!;
+            List<Dictionary_Model> dictionary = snapshot.data!;
             return Center(
-              child: Text(word.word!),
+              child: Text(dictionary[0].word!),
             );
           }else{
             return Center(
--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,60 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
-
-import 'package:http/http.dart';
-//  [
-//     {
-//       "word": "hello",
-//       "phonetic": "həˈləʊ",
-//       "phonetics": [
-//         {
-//           "text": "həˈləʊ",
-//           "audio": "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
-//         },
-//         {
-//           "text": "hɛˈləʊ"
-//         }
-//       ],
-//       "origin": "early 19th century: variant of earlier hollo ; related to holla.",
-//       "meanings": [
-//         {
-//           "partOfSpeech": "exclamation",
-//           "definitions": [
-//             {
-//               "definition": "used as a greeting or to begin a phone conversation.",
-//               "example": "hello there, Katie!",
-//               "synonyms": [],
-//               "antonyms": []
-//             }
-//           ]
-//         },
-//         {
-//           "partOfSpeech": "noun",
-//           "definitions": [
-//             {
-//               "definition": "an utterance of ‘hello’; a greeting.",
-//               "example": "she was getting polite nods and hellos from people",
-//               "synonyms": [],
-//               "antonyms": []
-//             }
-//           ]
-//         },
-//         {
-//           "partOfSpeech": "verb",
-//           "definitions": [
-//             {
-//               "definition": "say or shout ‘hello’.",
-//               "example": "I pressed the phone button and helloed",
-//               "synonyms": [],
-//               "antonyms": []
-//             }
-//           ]
-//         }
-//       ]
-//     }
-//   ]
-
-// https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+import 'package:http/http.dart'; 
 
 import 'package:english_dictionary/models/word.dart';
 
@@ -64,9 +10,7 @@ class HttpService {
         Uri.parse("https://api.dictionaryapi.dev/api/v2/entries/en/$word");
     Response response = await get(url);
     if (response.statusCode == 200) {
-      var json = jsonDecode(response.body);
-      log(response.body);
-      return Word.fromJson(json);
+      
     } else {
       throw (response);
     }
