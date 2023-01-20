8b90d1a54f624608a0ffb6f8ed03d37dd3e26e66
Mustapha amin <mustaphaamin2003@gmail.com>
2023-01-20 04:38:13 +0100

    modified my custom textstyle

--- lib/widgets/textStyle.dart
+++ lib/widgets/textStyle.dart
@@ -1,10 +1,11 @@
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
-kTextStyle(double size, [bool isBold = false]) {
+kTextStyle(double size, [bool isBold = false, FontStyle fontstyle = FontStyle.normal]) {
   return GoogleFonts.raleway(
     fontSize: size,
     fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
     color: Colors.black,
+    fontStyle: FontStyle.italic
   );
 }
